29a503aa36772ca33ad8dd47ec9fd20d14e2f86b
Foxynero <adugyamfifoster@gmail.com>
2023-03-29 05:03:53 -0700

    Binary Trees

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -1,6 +1,12 @@
 #include "binary_trees.h"
 
 /**
- *
- *
+ * binary_tree_delete - define function
+ * @void: describe argument
+ * Return: 0 on success
  */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	return (0);
+}
